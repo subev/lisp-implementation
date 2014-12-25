a1f6dcc6d5db26f1fe0c1deda50459ac526e6a53
Petur Subev <petur.subev@gmail.com>
2014-12-26 01:02:29 +0200

    Add portable support for windows by adding preprocesor conditions and fake implementations

--- repl/repl.c
+++ repl/repl.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* if on windows machine*/
+#ifdef _WIN32
+
+#include <string.h>
+static char* buffer[2048];
+
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\n';
+    return cpy;
+}
+
+void add_history(char* useless) {}
+
+#else
+
 #include <editline/readline.h>
 #include <editline/history.h>
+
+#endif
 
 int main(int argc, char** argv)
 {
